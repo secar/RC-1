269ef8746bce7c94e806645a81720c1956064c63
edeak <edeak@null.net>
2018-10-05 20:41:37 +0100

    NewHandler highly improved. File executes and waits on select.

--- cs/NewHandler.py
+++ cs/NewHandler.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 
-from socket import socket
+from socket import * 
 from select import select
 
 class NewHandler:
-    def setupTCP(self, port):
-        self.tcp_socket = socket(AF_INET, SOCK_STREAM)
-        self.tcp_socket.setblocking(0)
-        self.tcp_socket.bind((gethostname(), port))
-        self.tcp_socket.listen(20)
-
-    def setupUDP(self, port):
-        self.udp_socket = socket(AF_INET, SOCK_DGRAM)
-        self.udp_socket.setblocking(0)
-        self.udp_socket.bind((gethostname(), port))
-
     def __init__(self, port):
+        self.port = port
         self.setupUDP()
         self.setupTCP()
-        self.active_handlers = None
+        self.active_handlers = []
+
+    def setupTCP(self):
+        self.tcp_socket = socket(AF_INET, SOCK_STREAM)
+        self.tcp_socket.setblocking(0)
+        self.tcp_socket.bind((gethostname(), self.port))
+        self.tcp_socket.listen(20)
+
+    def setupUDP(self):
+        self.udp_socket = socket(AF_INET, SOCK_DGRAM)
+        self.udp_socket.setblocking(0)
+        self.udp_socket.bind((gethostname(), self.port))
 
     def acceptUDP(self):
-        addrinfo = self.udp_socket.recvfrom(0)[1] #XXX: may not work.  
+        addrinfo = self.udp_socket.getpeername()
         handler = BSHandler(addrinfo)
         self.active_handlers += handler
         self.setupUDP()
@@ -32,19 +33,25 @@ class NewHandler:
         self.active_handlers += handler
         self.setupTCP() 
 
-    def check_handlers(self):
+    def idle(self):
         # this select does not time out.
-        ready = select([self.tcp_socket, self.udp_socket], [], [])[0]
+        ready = select([h.get_socket() for h in self.active_handlers] + [self.tcp_socket, self.udp_socket], [], [])[0]
         if self.tcp_socket in ready:
-            acceptTCP()
+            self.acceptTCP()
         if self.udp_socket in ready:
-            acceptUDP()
+            self.acceptUDP()
         for handler in self.active_handlers:
-            if handler.dead:
-                active_handlers.remove(handler)
-            else:
-                handler.check()
+            closed = handler.handle()
+            if closed:
+                self.active_handlers.remove(handler)
         
     def start(self):
         while 1:
-            check_handlers()
+            self.idle()
+
+def main():
+    nh = NewHandler(50000)
+    nh.start()
+
+if __name__ == '__main__':
+    main()
