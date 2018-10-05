b46d1c1519bd64292ca58547bfe938ff5ee7a433
edeak <edeak@null.net>
2018-10-05 15:15:20 +0100

    new UserHandler class; improved class structure; no actual function yet.

--- cs.py
+++ cs.py
@@ -1,90 +1,119 @@
 #!/usr/bin/env python3
 
+'''
+	'addr': IP or hostname.
+	'addrinfo': tuple in the form ('addr', port).
+'''
+
 from socket import socket
 from sys import argv
 from time import sleep
 from select import select
 
-SELECT_TIMEOUT = 1
+SELECT_TIMEOUT = 0
 BUFFER_SIZE = 1024
 
-# with 'user'
-class UserHandler:
-   def __init__(self, socket, peer_addr):
+class Handler:
+    # A handler is ready if its socket is ready to write.
+    def __init__(self):
+        dead = False
+    def ready(self):
+        return select([self.socket], [], [], SELECT_TIMEOUT)[0]
+
+# with 'user'. Uses TCP only.
+class UserHandler(Handler):
+   def __init__(self, socket):
+        # socket passed already be bound and connected (as returned by accept())
         self.socket = socket
-        self.peer_addr = peer_addr
-    def sendAUR():
-        return NotImplemented
-    def sendDLR():
-        return NotImplemented
-    def sendBKR():
-        return NotImplemented
-    def sendRSR():
-        return NotImplemented
-    def sendLDR():
-        return NotImplemented
-    def sendLFD():
-        return NotImplemented
-    def sendDDR():
-        return NotImplemented
-    def parse():
-        return NotImplemented
+    def sendAUR(self):
+        raise NotImplementedError
+    def sendDLR(self):
+        raise NotImplementedError
+    def sendBKR(self):
+        raise NotImplementedError
+    def sendRSR(self):
+        raise NotImplementedError
+    def sendLDR(self):
+        raise NotImplementedError
+    def sendLFD(self):
+        raise NotImplementedError
+    def sendDDR(self):
+        raise NotImplementedError
+    def parse(self):
+        raise NotImplementedError
+    def idle(self):
+        raise NotImplementedError
+    def check(self):
+        if self.ready(self)
+            handle()
 
 # with 'BS'. Uses UDP only.
-class BSHandler:
-    def __init__(self, socket, peer_addr):
-        self.socket = socket
-        self.peer_addr = peer_addr
-    def sendRGR():
-        return NotImplemented
-    def sendUAR():
-        return NotImplemented
-    def sendLSF():
-        return NotImplemented
-    def sendLSU():
-        return NotImplemented
-    def sendDLB():
-        return NotImplemented
-    def parse():
-        return NotImplemented
-    def handle():
-        return NotImplemented
+class BSHandler(Handler):
+    def __init__(self, peer_addrinfo):
+        self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.socket.bind(peer_addrinfo)
+    def sendRGR(self):
+        raise NotImplementedError
+    def sendUAR(self):
+        raise NotImplementedError
+    def sendLSF(self):
+        raise NotImplementedError
+    def sendLSU(self):
+        raise NotImplementedError
+    def sendDLB(self):
+        raise NotImplementedError
+    def parse(self):
+        raise NotImplementedError
+    def idle(self):
+        raise NotImplementedError
+    def handle(self):
+        raise NotImplementedErroR
 
-class Waiter:
+class NewHandler:
     def setupTCP(self, port):
         self.tcp_socket = socket(AF_INET, SOCK_STREAM)
+        self.tcp_socket.setblocking(0)
         self.tcp_socket.bind((gethostname(), port))
         self.tcp_socket.listen(20)
 
     def setupUDP(self, port):
         self.udp_socket = socket(AF_INET, SOCK_DGRAM)
+        self.udp_socket.setblocking(0)
         self.udp_socket.bind((gethostname(), port))
 
     def __init__(self, port):
         self.setupUDP()
         self.setupTCP()
+        self.active_handlers = None
 
-    def UDPhandle(self): # XXX
-        addrinfo = self.udp_socket.recvfrom(0)[1]
-        handler = BSHandler(
+    def acceptUDP(self):
+        addrinfo = self.udp_socket.recvfrom(0)[1] #XXX: may not work.  
+        handler = BSHandler(addrinfo)
+        self.active_handlers += handler
         self.setupUDP()
 
-    def TCPhandle(self):
-        comm_socket, addrinfo = self.tcp_socket.accept()
-        handler = UserHandler(comm_socket, addr_info)
-        handler.start()
+    def acceptTCP(self):
+        comm_socket = self.tcp_socket.accept()[0]
+        handler = UserHandler(comm_socket)
+        self.active_handlers += handler
         self.setupTCP() 
 
-    def service_loop(self):
-        while 1:
-            ready = select([self.tcp_socket, self.udp_socket], [], [], SELECT_TIMEOUT)[0]
-            if self.tcp_socket in ready:
-                self.TCPhandle()
-            if self.udp_socket in ready:
-                self.UDPhandle()
-
+    def check_handlers(self):
+        # this select does not time out.
+        ready = select([self.tcp_socket, self.udp_socket], [], [])[0]
+        if self.tcp_socket in ready:
+            acceptTCP()
+        if self.udp_socket in ready:
+            acceptUDP()
+        for handler in self.active_handlers:
+            if handler.dead:
+                active_handlers.remove(handler)
+            else:
+                handler.check()
+        
     def start(self):
-        self.service_loop()
+        while 1:
+            check_handlers()
 
 def main():
     if '-p' in argv:
@@ -92,8 +121,8 @@ def main():
     else:
         port = 58056 
     print('Using port', port)
-    waiter = Waiter(port)
-    waiter.start()
+    NewHandler = nh(port)
+    nh.start()
  
 if __name__ == '__main__':
     main()
